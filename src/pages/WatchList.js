a6cb74577a7a17397c6463ebab9ebd3311db9381
Susan Dangol <49741990+susan5591@users.noreply.github.com>
2022-03-06 10:46:45 +0545

    set-to-array

--- src/pages/WatchList.js
+++ src/pages/WatchList.js
@@ -4,11 +4,16 @@ import '../css/poster.css'
 
 const WatchList = () => {
   const {watch} =  useContext(AppContext)
-  console.log(watch)
+  const newData = new Set(watch.map((item)=>item))
+  // console.log(newData)
+  const arr = Array.from(newData)
+
+  console.log(arr)
+
   return (
     <div className='poster'>
       {
-        watch.map((item)=>{
+        arr.map((item)=>{
           return <div className='poster-under'>
               <img src={item.poster} alt='watchlist poster' />
             </div>
